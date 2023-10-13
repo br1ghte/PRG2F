25b40c60f8516d7911006063699f6cf4ea01408e
josef.novy <josef.novy@infis.cz>
2023-10-13 12:05:44 +0200

    priprava na cviceni loops -A2

--- src/cvika/Practiceloops.java
+++ src/cvika/Practiceloops.java
@@ -3,6 +3,7 @@ package cvika;
 public class Practiceloops {
 
     public static void main(String[] args) {
-        System.out.println("Priprava na cviceni cyklu");
+
+        System.out.println("Priprava na cviceni loops - A2");
     }
 }
