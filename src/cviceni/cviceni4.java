57bb63a2c778ee8a41f613519110de3ee5f1ee28
josef.novy <josef.novy@infis.cz>
2023-11-03 10:48:52 +0100

    cteni, kolikrat se opakovalo cislo 42 v generaci 100000 cisel do 100

--- src/cviceni/cviceni4.java
+++ src/cviceni/cviceni4.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class cviceni4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int random = (int) ((Math.random()*101)*100000);
-        while (true) {
-            if (random == 4);
+        int random = 0;
+        int pocet = 0;
+        for (int i = 0; i < 100000; i++) {
+            random = (int) (Math.random()*100);
+            if (random == 42) {
+                pocet = pocet + 1;
+            }
         }
-
+        System.out.println("42 se opakovalo " + pocet + "x");
     }
 }
