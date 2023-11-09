f31a7a91dce9a7515653e4712a29f1281818900f
josef.novy <josef.novy@infis.cz>
2023-11-09 13:10:05 +0100

    jak generovat zaporna cisla (nedokonceno)

--- src/arrays/RandomIntervals.java
+++ src/arrays/RandomIntervals.java
@@ -2,6 +2,7 @@ package arrays;
 
 public class RandomIntervals {
     public static void main(String[] args) {
-        int random = (int)(Math.random()*51+50);
+        int random = (int)(Math.random()*);
+
     }
 }
