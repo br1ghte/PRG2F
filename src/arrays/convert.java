fbf3a7a311bd16888e5c087fcd2329c6cc7bd2e1
josef.novy <josef.novy@infis.cz>
2023-12-07 13:06:46 +0100

    naprava chyb v podobe vymazani nebo uziti //

--- src/arrays/convert.java
+++ src/arrays/convert.java
@@ -1,14 +1,1 @@
-//package arrays;
-//
-//public class convert {
-//   public static void main(String[] args) {
-//       int[] binaryArray = {1,0,1,1,1,1,0,1};
-//       int result = 0;
-//
-//       for (int i = 0; i < binaryArray.length; i++) {
-//           result += binaryArray[i] * (int)(Math.pow(2,i));
-//       }
-//
-//
-//    }
-/}
+
