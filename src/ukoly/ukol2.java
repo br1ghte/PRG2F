b43374dfba0636cbefa3c175b37711b19d403453
josef.novy <josef.novy@infis.cz>
2024-02-13 11:58:52 +0100

    neco na test, nevim jak se to dela

---
+++ src/ukoly/ukol2.java
@@ -0,0 +1,14 @@
+package ukoly;
+
+import java.util.Scanner;
+
+public class ukol2 {
+    public static void main(String[] args) {
+        int rows = 5;
+        int cols = 5;
+        int[][] celacisla = new int[rows][cols];
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
