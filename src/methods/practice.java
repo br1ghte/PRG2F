03194133819b48de231ec1a29cbc1e82d00ef1a4
josef.novy <josef.novy@infis.cz>
2024-03-08 12:30:39 +0100

    pridelani hodu kostek a dodelani vypisu casu podle sekund

--- src/methods/practice.java
+++ src/methods/practice.java
@@ -21,11 +21,29 @@ public class practice {
 
     //time convert
     public static String minuteConvert(int seconds){
-        String time;
-        int min;
-        int sec;
+        //String time;
+        //int hours = seconds / 3600;
+        //int remainingSeconds = seconds - hours * 3600;
+        //int min = remainingSeconds / 60;
+        //int sec = remainingSeconds % 60;
 
-        return time;
+        // allternative
+        // sec = seconds - (min*60)
+
+        //jeste jinak
+        int hours = seconds / 3600;
+        int min = (seconds % 3600) / 60;
+        int sec = seconds % 60;
+        return hours + ":" + min + ":" + sec;
+    }
+    
+    static int dieRolls(int diceAmount, int value) {
+        int sum = 0;
+        for (int i = 0; i < diceAmount; i++) {
+            sum = (int) (Math.random() * value + 1);
+
+        }
+        return sum;
     }
 
     public static int[] randomArray(int lenght, int min, int max){
@@ -43,6 +61,6 @@ public class practice {
         for (int i = 0; i < 10; i++) {
             System.out.println(Arrays.toString(randomArray(10, 50, 100)));
         }
-
+        System.out.println(minuteConvert(3845));
     }
 }
