021c8493870dd63df8170ef26809cd923c2908cc
josef.novy <josef.novy@infis.cz>
2024-04-19 13:03:49 +0200

    test na oop

---
+++ src/testy/oop/ukol1.java
@@ -0,0 +1,44 @@
+package testy.oop;
+
+public class ukol1 {
+        int balance;
+        int debt;
+
+    public ukol1(int balance, int debt) {
+        this.balance = balance;
+        this.debt = debt;
+    }
+
+    final int PRICE_PER_ROLL = 150;
+
+        void bet(int balance, int tocky, int tocky2, int tocky3) {
+        tocky = (int) (Math.random() * 7 + 1);
+        tocky2 = (int) (Math.random() * 7 + 1);
+        tocky3 = (int) (Math.random() * 7 + 1);
+        ;
+
+        this.balance = balance;
+        if (tocky == tocky2 && tocky2 == tocky3 && this.balance >= PRICE_PER_ROLL) {
+            balance -= PRICE_PER_ROLL;
+            balance += 1500;
+        } else {
+            System.out.println("Nelze vsadit");
+        }
+
+    }
+        void getBalance() {
+            System.out.println("Na ucte je " + balance);
+        }
+
+        void loan(int amount) {
+            balance += amount;
+            debt += (int) (amount * 1.5);
+
+
+        }
+
+    public static void main(String[] args) {
+            ukol1 e = new ukol1(300, 0);
+            e.getBalance();
+    }
+}
---
+++ src/testy/oop/ukol2.java
@@ -0,0 +1,30 @@
+package testy.oop;
+
+public class ukol2 {
+    int COST_PER_SQUARE_METER = 300;
+
+    boolean farming;
+
+
+
+    int rent(int width, int lenght, int days) {
+        int cena = width * lenght * days * COST_PER_SQUARE_METER;
+
+        if (farming && days == 183) {
+            cena -= 100000;
+
+            if (cena <= 0) {
+                cena = 0;
+            }
+        }
+
+
+        return cena;
+    }
+
+    public static void main(String[] args) {
+        ukol2 p = new ukol2();
+        System.out.println("Cena bude: " + p.rent(400, 500, 158));
+    }
+
+}
