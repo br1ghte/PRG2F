3a15fd208bc5764ac3a0390b755d76165eb31979
josef.novy <josef.novy@infis.cz>
2024-06-13 12:14:42 +0200

    oop - countries

---
+++ src/oop/countries/City.java
@@ -0,0 +1,22 @@
+package oop.countries;
+
+public class City {
+    String name;
+    int population;
+    Country country;
+
+    public City(String name, int population, Country country) {
+        this.name = name;
+        this.population = population;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Mesto " + name + " lezi v zemi " + country.name;
+    }
+
+    String getLanguageName(){
+        return this.country.language.languageName;
+    }
+}
---
+++ src/oop/countries/RandomIntervals.java
@@ -0,0 +1,10 @@
+package oop.countries;
+
+import javax.swing.*;
+public class RandomIntervals {
+    public static void main(String[] args) {
+        //-49, 50
+        int random = (int)(Math.random()*100 - 50);
+    }
+}
+
